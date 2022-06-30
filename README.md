a4d54e66a0dee0ce568bca002e5541b7e55e3bf9
Tero <tjankko@hotmail.com>
2022-06-30 06:56:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ModuleProtocols
+
+A description of this package.
---
+++ Sources/ModuleProtocols/ModuleProtocols.swift
@@ -0,0 +1,10 @@
+protocol ModuleProtocol {
+    func doSomething()
+}
+
+public struct ModuleProtocols {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
